9a44a6d6b00d91d3fcdea59e0416081f66b3b824
FelixOz13 <94942094+FelixOz13@users.noreply.github.com>
2022-12-02 03:54:01 -0600

    front-end inauguration

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -10,19 +10,20 @@ import Navbar from "./Navbar"
 function Intro() {
   return (
     <div>
-      <div className="hero2">
+       <div className="hero">
       <Navbar />
       
       <div>
         <div className="notranslate">
         <h1 className="title2">Bandera Musical</h1>
-        </div>
+          </div>
+         
         <h4 className="headertext"> Hola Paisano, Bienvenido a Bandera Musical!, Escoje el tipo de musica que mas te gusta`???</h4>
         
          <ButtonTreeCategories/>
         </div>
-        </div>
-      
+        
+      <img className="indianaut" src="./images/indianaut.png" alt=""/>
       <video
           className="player-wrapper-3"
           width="100%"
@@ -32,8 +33,9 @@ function Intro() {
           muted={true}
           playsInline={true}>
          <source src="./images/Banderamusicalgreenspace.mp4" type="video/mp4"/>
-      </video>
-      
+        </video>
+        <img className="indianaut" src="./images/indianaut.png" alt=""/>
+      </div>
         
    
         
