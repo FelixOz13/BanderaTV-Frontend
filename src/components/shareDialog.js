182c8d8e18a9438538633cf6b0c75ca60d6f75b5
Felix Ozuna <ozunafelix019@gmail.com>
2023-05-30 02:24:33 -0600

    database commit

--- src/components/shareDialog.js
+++ src/components/shareDialog.js
@@ -1,38 +1,29 @@
 import React, { useState } from 'react'
 import { FaShare } from 'react-icons/fa'
 
-const ShareDialog = ({ imageUrl, item }) => {
+const ShareDialog = ({ item }) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false)
 
   const handleShare = () => {
     const shareUrl = `${window.location.origin}`
-    const message = `Te Invitamos a disfrutar de  Bandera Musical`
+    const message = `Te Invitamos a disfrutar de Bandera Musical`
 
-    // Use the sharing functionality of the specific social media platform
-    // For example, you can use different libraries for sharing on Facebook, Twitter, etc.
-    // Here, we're using a simple window.open() to simulate sharing
+    const imageUrl = `${window.location.origin}/flyingsungod.jpg`
+    const shareContent = `${message}\n\n${imageUrl}`
 
-    // Example for Facebook sharing
-    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-      shareUrl,
-    )}`
-    window.open(facebookUrl)
-
-    // Example for Twitter sharing
-    const twitterUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
-      shareUrl,
-    )}&text=${encodeURIComponent(message)}`
-    window.open(twitterUrl)
-
-    // Close the dialog after sharing
-    setIsDialogOpen(false)
+    if (navigator.share) {
+      navigator
+        .share({ title: document.title, text: shareContent, url: shareUrl })
+        .then(() => console.log('Share successful'))
+        .catch((error) => console.error('Error sharing:', error))
+    } else {
+      console.warn('Web Share API not supported')
+    }
   }
 
   return (
     <div>
       <div className="share-dialog">
-        <img src={imageUrl} />
-
         <a className="sharebutton1" onClick={handleShare}>
           <FaShare />
         </a>
