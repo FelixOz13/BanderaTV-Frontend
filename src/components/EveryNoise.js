803a75b06c7aa6b208f34db918b1febef2f6eb4e
Felix Ozuna <ozunafelix019@gmail.com>
2023-08-27 18:45:30 -0600

    db  commit

--- src/components/EveryNoise.js
+++ src/components/EveryNoise.js
@@ -2,11 +2,20 @@ import { GiMusicalNotes } from 'react-icons/gi'
 
 function EveryNoise() {
   return (
-    <div>
+    <div className="socios">
+      <h5>"Socios en Colaboración"</h5>
+      <h6>Enlaces</h6>
       <a className="everynoisebutton" href="https://everynoise.com/">
         <h6>
           <GiMusicalNotes />
-          Link to Every Noise Radio Station
+          Every Noise Music Station
+          <GiMusicalNotes />
+        </h6>
+      </a>
+      <a className="gigmeoutbutton" href="https://gigmeout.io/en/">
+        <h6>
+          <GiMusicalNotes />
+          Gigmeout Social Media for Local Musicians
           <GiMusicalNotes />
         </h6>
       </a>
