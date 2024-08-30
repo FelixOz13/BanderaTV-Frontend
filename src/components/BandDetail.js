d8aef0a30fa568121a8be89c6ee71acdcbe42dfb
Felix Ozuna <ozunafelix019@gmail.com>
2024-08-29 21:40:44 -0600

    database

--- src/components/BandDetail.js
+++ src/components/BandDetail.js
@@ -53,28 +53,24 @@ const BandDetail = () => {
     return <div>Band not found</div>;
   }
   const encodedTitle = encodeURIComponent(band.title);
-  const shareUrl = `${window.location.origin}/Bandera/${encodedTitle}`;
-  const coverImgUrl = `${window.location.origin}/images/${band.coverImg}`;
- 
-  const handleShare = () => {
-    if (navigator.share) {
-      const shareContent = {
-        title: document.title,
-        text: `Te invitamos a disfrutar de ${band.title} con Bandera Musical.`,
-        url: shareUrl,
-        files: [
-          new File([coverImgUrl], `${band.title}.jpg`, { type: 'image/jpeg' })
-        ]
-      };
-  
-      navigator
-        .share(shareContent)
-        .then(() => console.log('Share successful'))
-        .catch(error => console.error('Error sharing:', error));
-    } else {
-      console.warn('Web Share API not supported');
-    }
-  };
+const shareUrl = `${window.location.origin}/Bandera/${encodedTitle}`;
+const coverImgUrl = `${window.location.origin}/images/${band.coverImg}`;
+
+const handleShare = () => {
+if (navigator.share) {
+const shareContent = {
+title: document.title,
+text: `Te Invitamos a disfrutar de ${band.title} con Bandera Musical. ${coverImgUrl}`,
+url: shareUrl, // Include the URL so it can be opened directly
+};
+navigator
+.share(shareContent)
+.then(() => console.log('Share successful'))
+.catch(error => console.error('Error sharing:', error));
+} else {
+console.warn('Web Share API not supported');
+}
+};
 
   
   return (
