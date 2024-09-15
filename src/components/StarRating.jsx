f8a72fe0aee388dd71442cdffdd1cffbb1d97813
Felix Ozuna <ozunafelix019@gmail.com>
2024-09-14 20:18:31 -0600

    db

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactStars from 'react-rating-stars-component';
 
-function StarRating({ size = 35, isHalf = true, count = 5 }) {
+function StarRating({ size = 25, isHalf = true, count = 5 }) {
   // Function to generate a random value between 2.5 and 5
   const getRandomRating = () => {
     return (Math.random() * 2.5) + 2.5;
